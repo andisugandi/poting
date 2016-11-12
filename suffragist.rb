96b16fda63a867e649e8883a4f0bf9e8e34c614c
Andi Sugandi <andisugandi@gmail.com>
2016-11-12 15:21:52 +0700

    Add YAML::Store to persist the result/data

--- suffragist.rb
+++ suffragist.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'yaml/store'
 
 Choices = {
   'HAM' => 'Hamburger',
@@ -16,11 +17,18 @@ end
 post '/cast' do
   @title = 'Thanks for casting your vote!'
   @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
   erb :cast
 end
 
 get '/results' do
-  @title = 'Vote Results!'
-  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3 }
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
   erb :results
 end
